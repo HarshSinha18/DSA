d460a1d16d5d8bf83f84374794fe0fcb169f9732
Harsh Sinha <harshsinha.sd.18@gmail.com>
2022-04-05 17:02:30 +0530

    Add files via upload

---
+++ Arrays/Basics/GFG.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class GFG {
+	public static void main(String[] args){
+		 Scanner sc= new Scanner(System.in);
+		  int n = sc.nextInt();
+		  int arr[] = new int[n];
+		  for(int i = 0 ; i< n ; i++) {
+			  arr[i] = sc.nextInt();
+		  }
+		  int k = sc.nextInt();
+		   ques6(arr,n , k);
+		
+	     }
+	public static void ques6(int arr[] , int n , int k) {
+		Arrays.sort(arr);
+		int largest = arr[k-1];
+		int minimum = arr[n-k];
+		System.out.println("largest : "+largest);
+		System.out.println("largest : "+minimum);
+		
+		
+	    }
+    }
