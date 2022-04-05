668edcb325a96c2852245fd73d854d2581883ac8
Harsh Sinha <harshsinha.sd.18@gmail.com>
2022-04-05 20:43:10 +0530

    Update Ques8.java

--- Arrays/Basics/Ques8.java
+++ Arrays/Basics/Ques8.java
@@ -1,3 +1,5 @@
+//  Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algorithm.
+
 import java.util.*;
 public class GFG {
 	public static void main(String[] args){
@@ -24,12 +26,15 @@ public class GFG {
 		}
 		for(int i  =0 ; i<zero ; i++) {
 			System.out.print("0 ");
+			// arr[i]=0;  -> also can do
 		}
 		for(int j = zero ; j<one+zero ; j++) {
 			System.out.print("1 ");
+			// arr[j]=1;
 		}
 		for(int k = zero+one ; k< n ; k++) {
 			System.out.print("2 ");
+			// arr[k]=2;
 		}
 	}
 }
