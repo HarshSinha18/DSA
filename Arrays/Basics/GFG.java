9e0d5349f898ee588c4b9d17b635c4e53b120374
Harsh Sinha <harshsinha.sd.18@gmail.com>
2022-04-05 16:10:23 +0530

    Add files via upload

---
+++ Arrays/Basics/GFG.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class GFG {
+	public static void main(String[] args){
+		 Scanner sc= new Scanner(System.in);
+		  int n = 10;
+		  int arr[] = new int[n];
+		  for(int i = 0 ; i < n ; i++) {
+			  arr[i] = sc.nextInt();
+		  }
+		  for(int i = 0 ; i< n ; i++) {
+			  System.out.print(arr[i]+" ");
+		  }
+	     }
+    }
