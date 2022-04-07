3e0da7e01066bf9b657624c345eedd626e61d0fd
Harsh Sinha <harshsinha.sd.18@gmail.com>
2022-04-07 13:21:57 +0530

    Update and rename GFG.java to Ques3.java

--- Arrays/Intermidiate/Ques3.java
+++ Arrays/Intermidiate/Ques3.java
@@ -1,3 +1,6 @@
+//You are given a list of n-1 integers and these integers are in the range of 1 to n. 
+//There are no duplicates in the list. One of the integers is missing in the list. Write an efficient code to find the missing integer.
+
 import java.util.*;
 public class GFG {
 	public static void main(String[] args){
