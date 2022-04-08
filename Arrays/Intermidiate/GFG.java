29f434921632f9e3a0c311dd8f25e3d5b515b097
Harsh Sinha <harshsinha.sd.18@gmail.com>
2022-04-08 10:32:18 +0530

    Add files via upload

---
+++ Arrays/Intermidiate/GFG.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class GFG{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+			arr[i] = sc.nextInt();
+	    	}
+		int ans = allDuplicate(arr,n);
+		System.out.println(ans);
+	}
+	public static int allDuplicate(int arr[] , int n) {
+		int count=1;
+		for(int i  =0 ; i< n ; i++) {
+			for(int j = i+1 ; j< n ; j++) {
+				//System.out.println(arr[i]+" "+arr[j]);
+				if(arr[i]==arr[j]) {
+					count++;
+				}
+			}
+			if(count>=2){
+				return i;
+			}
+		}
+		return -1;
+          
+	}
+
+}
